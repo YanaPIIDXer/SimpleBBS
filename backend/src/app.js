95cbf10d7e455cbc36c1c4b40ae5415ea3c91c8a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-27 05:02:27 +0900

    ログ出力のミドルウェアを突っ込む位置を変更

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,11 +4,12 @@ const { logInfo, logError } = require("./logging/logging.application.js");
 const app = express();
 const PORT = 3000;
 
-app.use(logError());
-
 app.get("/", (req, res) => {
+    throw new Error("Fuck!!");
     res.json({ message: "Hello, World!" });
 });
+
+app.use(logError());
 
 app.listen(PORT, () => {
     logInfo(`Start listen in PORT:${PORT}`);
