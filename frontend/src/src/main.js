e906e669c962a1dabb91d740c63a812329249d87
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-27 07:26:32 +0900

    Vuex導入

--- frontend/src/src/main.js
+++ frontend/src/src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
