635f0e7bd2bc48ef5a9b7064625f4c3dc57b4654
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-27 07:59:52 +0900

    テスト用の諸々を撤去

--- frontend/src/src/components/Top.vue
+++ frontend/src/src/components/Top.vue
@@ -1,28 +1,11 @@
 <template>
   <div>
     <h1>会員制掲示板</h1>
-    <div v-if="!isLogIn()">
-      <button class="btn btn-primary" v-on:click="logIn()">ログイン</button>
-    </div>
-    <div v-else>
-      <button class="btn btn-secondary" v-on:click="logOut()">ログアウト</button>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  methods: {
-    isLogIn() {
-      return this.$store.getters.isLogIn;
-    },
-    logIn() {
-      this.$store.commit("logIn", {});
-    },
-    logOut() {
-      this.$store.dispatch("logOut");
-    }
-  }
 }
 </script>
 
