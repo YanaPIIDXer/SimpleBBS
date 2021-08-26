1cf7ea4d08c4a64a0f044bcb90c86ce5a76461f0
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-27 07:57:44 +0900

    ・Storeの扱い方を正しくした ・ダミーのログイン・ログアウト

--- frontend/src/src/main.js
+++ frontend/src/src/main.js
@@ -1,13 +1,12 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
 import App from './App.vue'
+import store from './store.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-Vue.use(Vuex);
-
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
