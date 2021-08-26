364ae6fd7b3b5532d8b54f2036a4d3d808c7c9e5
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-27 08:09:05 +0900

    ナビゲーションバー とりあえずBootstrapの公式からパクってきて少し変えたもの

--- frontend/src/src/components/Top.vue
+++ frontend/src/src/components/Top.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
+    <NavBar />
     <h1>会員制掲示板</h1>
   </div>
 </template>
 
 <script>
+import NavBar from './NavBar.vue'
+
 export default {
+  name: "Top",
+  components: {
+    NavBar
+  }
 }
 </script>
 
